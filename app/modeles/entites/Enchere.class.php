3a1c76167adca7879b56f47d97fa3939cc87c25c
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-07-04 16:30:59 -0400

    first commit

---
+++ app/modeles/entites/Enchere.class.php
@@ -0,0 +1,324 @@
+<?php
+
+/**
+ * Classe de l'entité Enchere
+ *
+ */
+class Enchere extends Entite
+{
+  protected $ID;
+  protected $TimbreID;
+  protected $UtilisateurID;
+  protected $DateDebut;
+  protected $DateFin;
+  protected $PrixPlancher;
+  protected $UtilisateurActuelID;
+  protected $Visible;
+  protected $Status;
+  protected $Rating;
+
+  protected $TimbreNom;
+  protected $TimbreDateCreation;
+  protected $TimbreCouleur;
+  protected $TimbrePaysOrigine;
+  protected $TimbreEtatCondition;
+  protected $TimbreTirage;
+  protected $TimbreLongueur;
+  protected $TimbreLargeur;
+  protected $TimbreCertifie;
+  protected $CategorieID;
+
+  /**
+   * Mutateur de la propriété ID
+   * @param int $ID
+   * @return $this
+   */
+  public function setID($ID) {
+    unset($this->erreurs['ID']);
+    $regExp = '/^[1-9]\d*$/';
+    if (!preg_match($regExp, $ID)) {
+      $this->erreurs['ID'] = 'Numéro d\'enchère incorrect.';
+    }
+    $this->ID = $ID;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreID
+   * @param int $TimbreID
+   * @return $this
+   */
+  public function setTimbreID($TimbreID) {
+    unset($this->erreurs['TimbreID']);
+    $regExp = '/^[1-9]\d*$/';
+    if (!preg_match($regExp, $TimbreID)) {
+      $this->erreurs['TimbreID'] = 'Numéro de timbre incorrect.';
+    }
+    $this->TimbreID = $TimbreID;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété UtilisateurID
+   * @param int $UtilisateurID
+   * @return $this
+   */
+  public function setUtilisateurID($UtilisateurID) {
+    unset($this->erreurs['UtilisateurID']);
+    $regExp = '/^[1-9]\d*$/';
+    if (!preg_match($regExp, $UtilisateurID)) {
+      $this->erreurs['UtilisateurID'] = 'Numéro d\'utilisateur incorrect.';
+    }
+    $this->UtilisateurID = $UtilisateurID;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété DateDebut
+   * @param string $DateDebut
+   * @return $this
+   */
+  public function setDateDebut($DateDebut) {
+    unset($this->erreurs['DateDebut']);
+    if (!strtotime($DateDebut)) {
+      $this->erreurs['DateDebut'] = 'Date de début invalide.';
+    }
+    $this->DateDebut = $DateDebut;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété DateFin
+   * @param string $DateFin
+   * @return $this
+   */
+  public function setDateFin($DateFin) {
+    unset($this->erreurs['DateFin']);
+    if (!strtotime($DateFin)) {
+      $this->erreurs['DateFin'] = 'Date de fin invalide.';
+    }
+    $this->DateFin = $DateFin;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété PrixPlancher
+   * @param float $PrixPlancher
+   * @return $this
+   */
+  public function setPrixPlancher($PrixPlancher) {
+    unset($this->erreurs['PrixPlancher']);
+    if (!is_numeric($PrixPlancher) || $PrixPlancher < 0) {
+      $this->erreurs['PrixPlancher'] = 'Prix plancher invalide.';
+    }
+    $this->PrixPlancher = $PrixPlancher;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété UtilisateurActuelID
+   * @param int $UtilisateurActuelID
+   * @return $this
+   */
+  public function setUtilisateurActuelID($UtilisateurActuelID) {
+    unset($this->erreurs['UtilisateurActuelID']);
+    $regExp = '/^[1-9]\d*$/';
+    if (!preg_match($regExp, $UtilisateurActuelID)) {
+      $this->erreurs['UtilisateurActuelID'] = 'Numéro d\'utilisateur actuel incorrect.';
+    }
+    $this->UtilisateurActuelID = $UtilisateurActuelID;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété Visible
+   * @param bool $Visible
+   * @return $this
+   */
+  public function setVisible($Visible) {
+    unset($this->erreurs['Visible']);
+    if (!is_bool($Visible)) {
+      $this->erreurs['Visible'] = 'La visibilité doit être un booléen.';
+    }
+    $this->Visible = $Visible;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété Status
+   * @param string $Status
+   * @return $this
+   */
+  public function setStatus($Status) {
+    unset($this->erreurs['Status']);
+    $statusOptions = array('En cours', 'Terminée', 'Annulée');
+    if (!in_array($Status, $statusOptions)) {
+      $this->erreurs['Status'] = 'Statut invalide.';
+    }
+    $this->Status = $Status;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété Rating
+   * @param float $Rating
+   * @return $this
+   */
+  public function setRating($Rating) {
+    unset($this->erreurs['Rating']);
+    if (!is_numeric($Rating) || $Rating < 0 || $Rating > 5) {
+      $this->erreurs['Rating'] = 'Évaluation invalide.';
+    }
+    $this->Rating = $Rating;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreNom
+   * @param string $TimbreNom
+   * @return $this
+   */
+  public function setTimbreNom($TimbreNom) {
+    unset($this->erreurs['TimbreNom']);
+    $TimbreNom = trim($TimbreNom);
+    $regExp = '/^.+$/';
+    if (!preg_match($regExp, $TimbreNom)) {
+      $this->erreurs['TimbreNom'] = 'Au moins un caractère.';
+    }
+    $this->TimbreNom = $TimbreNom;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreDateCreation
+   * @param string $TimbreDateCreation
+   * @return $this
+   */
+  public function setTimbreDateCreation($TimbreDateCreation) {
+    unset($this->erreurs['TimbreDateCreation']);
+    if (!strtotime($TimbreDateCreation)) {
+      $this->erreurs['TimbreDateCreation'] = 'Date de création invalide.';
+    }
+    $this->TimbreDateCreation = $TimbreDateCreation;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreCouleur
+   * @param string $TimbreCouleur
+   * @return $this
+   */
+  public function setTimbreCouleur($TimbreCouleur) {
+    unset($this->erreurs['TimbreCouleur']);
+    $TimbreCouleur = trim($TimbreCouleur);
+    $regExp = '/^.+$/';
+    if (!preg_match($regExp, $TimbreCouleur)) {
+      $this->erreurs['TimbreCouleur'] = 'Au moins un caractère.';
+    }
+    $this->TimbreCouleur = $TimbreCouleur;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbrePaysOrigine
+   * @param string $TimbrePaysOrigine
+   * @return $this
+   */
+  public function setTimbrePaysOrigine($TimbrePaysOrigine) {
+    unset($this->erreurs['TimbrePaysOrigine']);
+    $TimbrePaysOrigine = trim($TimbrePaysOrigine);
+    $regExp = '/^.+$/';
+    if (!preg_match($regExp, $TimbrePaysOrigine)) {
+      $this->erreurs['TimbrePaysOrigine'] = 'Au moins un caractère.';
+    }
+    $this->TimbrePaysOrigine = $TimbrePaysOrigine;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreEtatCondition
+   * @param string $TimbreEtatCondition
+   * @return $this
+   */
+  public function setTimbreEtatCondition($TimbreEtatCondition) {
+    unset($this->erreurs['TimbreEtatCondition']);
+    $TimbreEtatCondition = trim($TimbreEtatCondition);
+    $regExp = '/^.+$/';
+    if (!preg_match($regExp, $TimbreEtatCondition)) {
+      $this->erreurs['TimbreEtatCondition'] = 'Au moins un caractère.';
+    }
+    $this->TimbreEtatCondition = $TimbreEtatCondition;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreTirage
+   * @param int $TimbreTirage
+   * @return $this
+   */
+  public function setTimbreTirage($TimbreTirage) {
+    unset($this->erreurs['TimbreTirage']);
+    if (!preg_match('/^[1-9]\d*$/', $TimbreTirage) || $TimbreTirage < 0) {
+      $this->erreurs['TimbreTirage'] = 'Tirage invalide.';
+    }
+    $this->TimbreTirage = $TimbreTirage;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreLongueur
+   * @param float $TimbreLongueur
+   * @return $this
+   */
+  public function setTimbreLongueur($TimbreLongueur) {
+    unset($this->erreurs['TimbreLongueur']);
+    if (!is_numeric($TimbreLongueur) || $TimbreLongueur < 0) {
+      $this->erreurs['TimbreLongueur'] = 'Longueur invalide.';
+    }
+    $this->TimbreLongueur = $TimbreLongueur;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreLargeur
+   * @param float $TimbreLargeur
+   * @return $this
+   */
+  public function setTimbreLargeur($TimbreLargeur) {
+    unset($this->erreurs['TimbreLargeur']);
+    if (!is_numeric($TimbreLargeur) || $TimbreLargeur < 0) {
+      $this->erreurs['TimbreLargeur'] = 'Largeur invalide.';
+    }
+    $this->TimbreLargeur = $TimbreLargeur;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété TimbreCertifie
+   * @param bool $TimbreCertifie
+   * @return $this
+   */
+  public function setTimbreCertifie($TimbreCertifie) {
+    unset($this->erreurs['TimbreCertifie']);
+    if (!is_bool($TimbreCertifie)) {
+      $this->erreurs['TimbreCertifie'] = 'La certification doit être un booléen.';
+    }
+    $this->TimbreCertifie = $TimbreCertifie;
+    return $this;
+  }
+
+  /**
+   * Mutateur de la propriété CategorieID
+   * @param int $CategorieID
+   * @return $this
+   */
+  public function setCategorieID($CategorieID) {
+    unset($this->erreurs['CategorieID']);
+    $regExp = '/^[1-9]\d*$/';
+    if (!preg_match($regExp, $CategorieID)) {
+      $this->erreurs['CategorieID'] = 'Numéro de catégorie incorrect.';
+    }
+    $this->CategorieID = $CategorieID;
+    return $this;
+  }
+}
---
+++ app/modeles/sql/RequetesSQL.class.php
@@ -0,0 +1,241 @@
+<?php
+
+/**
+ * Classe des requêtes SQL
+ *
+ */
+class RequetesSQL extends RequetesPDO {
+
+  /* GESTION DES UTILISATEURS 
+     ======================== */
+
+  /**
+   * Récupération des utilisateurs
+   * @return array tableau d'objets Utilisateur
+   */ 
+  public function getUtilisateurs() {
+    $this->sql = "
+      SELECT utilisateur_id, utilisateur_nom, utilisateur_prenom, utilisateur_courriel,
+             utilisateur_renouveler_mdp, utilisateur_profil
+      FROM utilisateur ORDER BY utilisateur_id DESC";
+     return $this->getLignes();
+  }
+
+  /**
+   * Récupération d'un utilisateur
+   * @param int $utilisateur_id, clé du utilisateur  
+   * @return array|false tableau associatif de la ligne produite par la select, false si aucune ligne
+   */ 
+  public function getUtilisateur($utilisateur_id) {
+    $this->sql = "
+      SELECT utilisateur_id, utilisateur_nom, utilisateur_prenom, utilisateur_courriel,
+             utilisateur_renouveler_mdp, utilisateur_profil
+      FROM utilisateur
+      WHERE utilisateur_id = :utilisateur_id";
+    return $this->getLignes(['utilisateur_id' => $utilisateur_id], RequetesPDO::UNE_SEULE_LIGNE);
+  }
+
+  /**
+   * Contrôler si adresse courriel non déjà utilisée par un autre utilisateur que utilisateur_id
+   * @param array $champs tableau utilisateur_courriel et utilisateur_id (0 si dans toute la table)
+   * @return array|false utilisateur avec ce courriel, false si courriel disponible
+   */ 
+  public function controlerCourriel($champs) {
+    $this->sql = 'SELECT utilisateur_id FROM utilisateur
+                  WHERE utilisateur_courriel = :utilisateur_courriel AND utilisateur_id != :utilisateur_id';
+    return $this->getLignes($champs, RequetesPDO::UNE_SEULE_LIGNE);
+  }
+
+  /**
+   * Connecter un utilisateur
+   * @param array $champs, tableau avec les champs utilisateur_courriel et utilisateur_mdp  
+   * @return array|false tableau associatif de la ligne produite par la select, false si aucune ligne
+   */ 
+  public function connecter($champs) {
+    $this->sql = "
+      SELECT utilisateur_id, utilisateur_nom, utilisateur_prenom,
+             utilisateur_courriel, utilisateur_renouveler_mdp, utilisateur_profil
+      FROM utilisateur
+      WHERE utilisateur_courriel = :utilisateur_courriel AND utilisateur_mdp = SHA2(:utilisateur_mdp, 512)";
+    return $this->getLignes($champs, RequetesPDO::UNE_SEULE_LIGNE);
+  }
+
+  /**
+   * Ajouter un utilisateur
+   * @param array $champs tableau des champs de l'utilisateur 
+   * @return int|string clé primaire de la ligne ajoutée, message d'erreur sinon
+   */ 
+  public function ajouterUtilisateur($champs) {
+    $utilisateur = $this->controlerCourriel(
+      ['utilisateur_courriel' => $champs['utilisateur_courriel'], 'utilisateur_id' => 0]);
+    if ($utilisateur !== false)
+      return Utilisateur::ERR_COURRIEL_EXISTANT;
+    $this->sql = '
+      INSERT INTO utilisateur SET
+      utilisateur_nom            = :utilisateur_nom,
+      utilisateur_prenom         = :utilisateur_prenom,
+      utilisateur_courriel       = :utilisateur_courriel,
+      utilisateur_mdp            = SHA2(:utilisateur_mdp, 512),
+      utilisateur_renouveler_mdp = "oui",
+      utilisateur_profil         = :utilisateur_profil';
+    return $this->CUDLigne($champs);
+  }
+
+  /**
+   * Créer un compte utilisateur dans le frontend
+   * @param array $champs tableau des champs de l'utilisateur 
+   * @return int|string clé primaire de la ligne ajoutée, message d'erreur sinon
+   */ 
+  public function creerCompteClient($champs) {
+    $utilisateur = $this->controlerCourriel(
+      ['utilisateur_courriel' => $champs['utilisateur_courriel'], 'utilisateur_id' => 0]);
+    if ($utilisateur !== false)
+      return ['utilisateur_courriel' => Utilisateur::ERR_COURRIEL_EXISTANT];
+    unset($champs['nouveau_mdp_bis']);  
+    $this->sql = '
+      INSERT INTO utilisateur SET
+      utilisateur_nom            = :utilisateur_nom,
+      utilisateur_prenom         = :utilisateur_prenom,
+      utilisateur_courriel       = :utilisateur_courriel,
+      utilisateur_mdp            = SHA2(:nouveau_mdp, 512),
+      utilisateur_renouveler_mdp = "non",
+      utilisateur_profil         = "'.Utilisateur::PROFIL_CLIENT.'"';
+    return $this->CUDLigne($champs);
+  }
+
+
+  /**
+   * Modifier un utilisateur
+   * @param array $champs tableau des champs de l'utilisateur 
+   * @return boolean|string true si modifié, message d'erreur sinon
+   */ 
+  public function modifierUtilisateur($champs) {
+    $utilisateur = $this->controlerCourriel(
+      ['utilisateur_courriel' => $champs['utilisateur_courriel'], 'utilisateur_id' => $champs['utilisateur_id']]);
+    if ($utilisateur !== false)
+      return Utilisateur::ERR_COURRIEL_EXISTANT;
+    $this->sql = '
+      UPDATE utilisateur SET
+      utilisateur_nom      = :utilisateur_nom,
+      utilisateur_prenom   = :utilisateur_prenom,
+      utilisateur_courriel = :utilisateur_courriel,
+      utilisateur_profil   = :utilisateur_profil
+      WHERE utilisateur_id = :utilisateur_id
+      AND utilisateur_id > 4'; // ne pas modifier les 4 premiers utilisateurs du jeu d'essai
+    return $this->CUDLigne($champs);
+  }
+
+  /**
+   * Modifier le mot de passe d'un utilisateur
+   * @param array $champs tableau des champs de l'utilisateur 
+   * @return boolean true si modifié, false sinon
+   */ 
+  public function modifierUtilisateurMdpGenere($champs) {
+    $this->sql = '
+      UPDATE utilisateur SET
+      utilisateur_mdp            = SHA2(:utilisateur_mdp, 512),
+      utilisateur_renouveler_mdp = "oui"
+      WHERE utilisateur_id = :utilisateur_id
+      AND utilisateur_id > 4'; // ne pas modifier les 4 premiers utilisateurs du jeu d'essai
+    return $this->CUDLigne($champs);
+  }
+
+  /**
+   * Modifier le mot de passe saisi d'un utilisateur
+   * @param array $champs tableau des champs de l'utilisateur 
+   * @return boolean true si modifié, false sinon
+   */ 
+  public function modifierUtilisateurMdpSaisi($champs) {
+    $this->sql = '
+      UPDATE utilisateur SET
+      utilisateur_mdp            = SHA2(:utilisateur_mdp, 512), 
+      utilisateur_renouveler_mdp = "non"
+      WHERE utilisateur_id = :utilisateur_id
+      AND utilisateur_id > 4'; // ne pas modifier les 4 premiers utilisateurs du jeu d'essai
+    return $this->CUDLigne($champs);
+  }
+
+  /**
+   * Supprimer un utilisateur
+   * @param int $utilisateur_id clé primaire
+   * @return boolean|string true si suppression effectuée, message d'erreur sinon
+   */ 
+  public function supprimerUtilisateur($utilisateur_id) {
+    $this->sql = '
+      DELETE FROM utilisateur WHERE utilisateur_id = :utilisateur_id
+      AND utilisateur_id NOT IN (SELECT DISTINCT f_r_film_id FROM film_realisateur)
+      AND utilisateur_id NOT IN (SELECT DISTINCT f_a_film_id FROM film_acteur)
+      AND utilisateur_id NOT IN (SELECT DISTINCT f_p_film_id FROM film_pays)
+      AND utilisateur_id NOT IN (SELECT DISTINCT seance_film_id FROM seance)
+      AND utilisateur_id > 4'; // ne pas supprimer les 4 premiers utilisateurs du jeu d'essai
+    return $this->CUDLigne(['utilisateur_id' => $utilisateur_id]);
+  }
+
+
+
+
+
+
+  /* GESTION DES ENCHERES 
+     ================= */
+
+  /**
+   * Récupération des encheres à l'affiche ou prochainement ou pour l'interface admin
+   * @param  string $critere
+   * @return array tableau des lignes produites par la select   
+   */ 
+  public function getEncheres() {
+    //$this->sql = "SELECT E.*, T.* FROM Enchere E INNER JOIN Timbre T ON E.TimbreID = T.ID ORDER BY E.DateFin DESC";
+
+
+    $this->sql = "SELECT
+    e.ID,
+    e.TimbreID,
+    e.UtilisateurID,
+    e.DateDebut,
+    e.DateFin,
+    e.PrixPlancher,
+    e.UtilisateurActuelID,
+    e.Visible,
+    e.Status,
+    e.Rating,
+    t.Nom AS TimbreNom,
+    t.DateCreation AS TimbreDateCreation,
+    t.Couleur AS TimbreCouleur,
+    t.PaysOrigine AS TimbrePaysOrigine,
+    t.EtatCondition AS TimbreEtatCondition,
+    t.Tirage AS TimbreTirage,
+    t.Longueur AS TimbreLongueur,
+    t.Largeur AS TimbreLargeur,
+    t.Certifie AS TimbreCertifie,
+    t.CategorieID,
+    COALESCE(mises.NombreMises, 0) AS NombreMises,
+    COALESCE(mises.PrixActuel, e.PrixPlancher) AS PrixActuel,
+    i.CheminImage AS PremiereImage
+FROM
+    Enchere e
+    JOIN Timbre t ON e.TimbreID = t.ID
+    LEFT JOIN (
+        SELECT
+            EnchereID,
+            COUNT(*) AS NombreMises,
+            MAX(Prix) AS PrixActuel
+        FROM
+            Offre
+        GROUP BY
+            EnchereID
+    ) mises ON e.ID = mises.EnchereID
+    LEFT JOIN (
+        SELECT
+            TimbreID,
+            CheminImage
+        FROM
+            Image
+        WHERE
+            Ordre = 1
+    ) i ON t.ID = i.TimbreID;";
+
+     return $this->getLignes();
+    }
+
+}
