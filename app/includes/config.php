74e9850ba9d22358210183069ff5337b0fd45089
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-07-14 15:56:22 -0400

    correction typos

--- app/includes/config.php
+++ app/includes/config.php
@@ -1,6 +1,6 @@
 <?php
   //define("ENV", "DEV");
-  define("ENV", "webdev");
-  //define("ENV", "DEV");
+  //define("ENV", "webdev");
+  define("ENV", "DEV");
 
   if (ENV === "DEV") define("MOCK_NOW", "2021-10-25");
