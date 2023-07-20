39b842d3920762544a2eac0b4e307059b78dbc75
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-07-20 11:31:24 -0400

    Magnify ok

--- js/encheres/vEnchereActions.js
+++ js/encheres/vEnchereActions.js
@@ -10,3 +10,90 @@ btnAjouterFavoris.addEventListener("click", (element) => {
   let appl = new EncheresApp();
   appl.toggleFavoris(element);
 });
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Gestion du magnify (loupe)
+magnify("imgTimbre", 3);
+
+
+/**
+ * Fonction pour agrandir l'image
+ * @param {*} imgID id de l'image
+ * @param {*} zoom zoom
+ */
+function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+  console.log(img);
+  /*Créer magnifier glass:*/
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+  /*Insérer magnifier glass:*/
+  img.parentElement.insertBefore(glass, img);
+  /*set background properties for the magnifier glass:*/
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize =
+    img.width * zoom + "px " + img.height * zoom + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+  /*execute a function when someone moves the magnifier glass over the image:*/
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+  /*and also for touch screens:*/
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+
+  function moveMagnifier(e) {
+    console.log("moveMagnifier");
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image*/
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /*prevent the magnifier glass from being positioned outside the image:*/
+    if (x > img.width - w / zoom) {
+      x = img.width - w / zoom;
+    }
+    if (x < w / zoom) {
+      x = w / zoom;
+    }
+    if (y > img.height - h / zoom) {
+      y = img.height - h / zoom;
+    }
+    if (y < h / zoom) {
+      y = h / zoom;
+    }
+    /*ajuster position*/
+    glass.style.left = x - w + "px";
+    glass.style.top = y - h + "px";
+    /*afficher contenu de la "loupe":*/
+    glass.style.backgroundPosition =
+      "-" + (x * zoom - w + bw) + "px -" + (y * zoom - h + bw) + "px";
+  }
+
+  /**
+   * Recupérer la position du curseur
+   * @param {*} e event(pour position)
+   * @returns {x,y} position du curseur
+   */
+  function getCursorPos(e) {
+    var a,
+      x = 0,
+      y = 0;
+    e = e || window.event;
+    /*get the x and y positions of the image:*/
+    a = img.getBoundingClientRect();
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return { x: x, y: y };
+  }
+}
